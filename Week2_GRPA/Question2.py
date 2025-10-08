c0631026ce3ab642351ca148bb614c0e1dfffbf5
KONAKALAPRASANTHI <prasanthikonakala16@gmail.com>
2025-10-08 19:25:53 +0530

    Question2.py

---
+++ Week2_GRPA/Question2.py
@@ -0,0 +1,16 @@
+'''' GrPA 2 - String Escaping - GRADED
+Note: There are no inputs to this question, you only have to print the things that are given in the comments in each line
+# Question
+Problem Type - Variable Out - Hidden suffix for input and output '''
+# Answer
+# A single quote ' and a double quote "
+output1 = ''' A single quote ' and a double quote " '''
+
+# A forward slash / and a backward slash \
+output2 = "A forward slash / and a backward slash \ "
+
+# Three single quotes ''' and three double quotes """
+output3 = ''' Three single quotes \''' and three double quotes """ '''
+
+# Double forward slash // and Double backward slash \\
+output4 = ''' Double forward slash // and Double backward slash \\\ '''
